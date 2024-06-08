8772dd55d7d712f70615e243bdab8a095c65b876
Eric Harrison <hericeh72@gmail.com>
2024-06-08 15:56:21 -0700

    Changing pages to display information from the database

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,3 +1,23 @@
-export default function Page() {
-    return <h1>Hello, Next.js!</h1>
-  }
+import { useEffect, useState } from 'react';
+
+export default function Home() {
+  const [user, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/users')
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error('Error fetching users:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {user.map((user, index) => (
+          <li key={index}>{user}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
