ba5cccc7569d45333738b5082de97af042daac4f
Eric Harrison <hericeh72@gmail.com>
2024-06-10 14:40:56 -0700

    Updating index test

--- app/__tests__/index.test.tsx
+++ app/__tests__/index.test.tsx
@@ -3,16 +3,15 @@ import { render, screen } from '@testing-library/react';
 import Page from '../../app/src/pages/index';
 import { describe, it } from 'node:test';
 // Import Jest types for TypeScript
-import { expect } from '@jest/globals';
+import '@testing-library/jest-dom/extend-expect';
 
 
-/*describe('Page', () => {
+describe('Page', () => {
   it('renders a heading', () => {
     render(<Page />);
 
     const heading: HTMLElement = screen.getByRole('heading', { level: 1 });
 
-    expect(heading).toBeDefined();
+    expect(heading).toBeInTheDocument();
   });
 });
-*/
--- app/jest.setup.ts
+++ app/jest.setup.ts
@@ -1,4 +1,5 @@
 import mysql from 'mysql2/promise';
+import '@testing-library/jest-dom'
 
 let pool: mysql.Pool;
 
