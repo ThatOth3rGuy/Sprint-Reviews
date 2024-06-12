4b0a8bb1787c7cd0c168245a0ffe186a7ff02c6c
Eric Harrison <hericeh72@gmail.com>
2024-06-11 18:21:31 -0700

    Adjusted page styling to match Figma prototype

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,11 +1,21 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import styles from '../styles/index.module.css'; // Assuming you have or will create a CSS module for styling
 
 export default function Page() {
   return (
     <div className={styles.roleSelection}>
       <header className={styles.roleButton}>Select Your Role</header>
-      <p>I am a</p>
+      <Image 
+        className={styles.image} 
+        src="/sprintrunner.png" 
+        alt="SprintRunners Logo"
+        width={363}
+        height={330}
+      />
+      <div className={styles.line}></div>
+      <p className={styles.description}>Choose from below to continue to sign up</p>
+      <p className={styles.iAmA}>I am a:</p>
       <div className={styles.roleButtons}>
         <Link href="/home/student">
           <button className={styles.studentButton}>Student</button>
