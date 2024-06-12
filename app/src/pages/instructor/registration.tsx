95bbdc3374c4606ca40e781db9c51246c2d19966
Eric Harrison <hericeh72@gmail.com>
2024-06-12 15:55:51 -0700

    Added functionality to back button

--- app/src/pages/instructor/registration.tsx
+++ app/src/pages/instructor/registration.tsx
@@ -15,66 +15,71 @@ const SignUp: NextPage = () => {
         // Handle sign-up logic here
         console.log('User signed up with:', { firstName, lastName, email, password });
       };
+
+    const handleBackClick = () => {
+        // Redirect user to login page
+        router.push('/instructor/login');
+      };
     
-      return (
-        <div className={styles.signUp}>
-          <img className={styles.sprintrunner1Icon} alt="Sprintrunner" src="/images/Logo.png" />
-          <div className={styles.signUpChild}></div>
-          <div className={styles.signUpItem}>
-            <div className={styles.createAnAccount}>Create an Account</div>
-          </div>
-          <div className={`${styles.textInput}`} style={{ top: '200px' }}>
-            <input
-              type="text"
-              placeholder="First Name"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-              className={styles.textInputChild}
-            />
-          </div>
-          <div className={`${styles.textInput}`} style={{ top: '270px' }}>
-            <input
-              type="text"
-              placeholder="Last Name"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-              className={styles.textInputChild}
-            />
-          </div>
-          <div className={`${styles.textInput}`} style={{ top: '340px' }}>
-            <input
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className={styles.textInputChild}
-            />
-          </div>
-          <div className={`${styles.textInput}`} style={{ top: '410px' }}>
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className={styles.textInputChild}
-            />
-          </div>
-          <div className={`${styles.textInput}`} style={{ top: '480px' }}>
-            <input
-              type="password"
-              placeholder="Confirm Password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              className={styles.textInputChild}
-            />
-          </div>
-          <button className={styles.createEvaluationButton} onClick={handleSignUpClick}>
-            <div className={styles.createEvaluationButtonChild}>Sign Up</div>
-          </button>
-          <p className={styles.pleaseEnterThe}>Please enter the following information:</p>
-          <img className={styles.backIcon} alt="Back" src="/images/Back-Arrow.png" />
+    return (
+      <div className={styles.signUp}>
+        <img className={styles.sprintrunner1Icon} alt="Sprintrunner" src="/images/Logo.png" />
+        <div className={styles.signUpChild}></div>
+        <div className={styles.signUpItem}>
+        <div className={styles.createAnAccount}>Create an Account</div>
         </div>
-      );
+        <div className={`${styles.textInput}`} style={{ top: '200px' }}>
+        <input
+          type="text"
+          placeholder="First Name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          className={styles.textInputChild}
+        />
+        </div>
+        <div className={`${styles.textInput}`} style={{ top: '270px' }}>
+        <input
+          type="text"
+          placeholder="Last Name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          className={styles.textInputChild}
+        />
+        </div>
+        <div className={`${styles.textInput}`} style={{ top: '340px' }}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className={styles.textInputChild}
+        />
+        </div>
+        <div className={`${styles.textInput}`} style={{ top: '410px' }}>
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className={styles.textInputChild}
+        />
+        </div>
+        <div className={`${styles.textInput}`} style={{ top: '480px' }}>
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          className={styles.textInputChild}
+        />
+        </div>
+        <button className={styles.createEvaluationButton} onClick={handleSignUpClick}>
+        <div className={styles.createEvaluationButtonChild}>Sign Up</div>
+        </button>
+        <p className={styles.pleaseEnterThe}>Please enter the following information:</p>
+        <img className={styles.backIcon} alt="Back" src="/images/Back-Arrow.png" onClick={handleBackClick}/>
+      </div>
+    );
     };
     
     export default SignUp;
