88d9afbd4477bf85fffd259468df5a5b7a76f9f2
Yatharth Mathur <yatharth711@gmail.com>
2024-06-13 12:09:51 -0700

    GoogleoAUTH API implemented on index.tsx for registration and login page

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import styles from '../styles/index.module.css'; // Assuming you have or will create a CSS module for styling
+import { GoogleLogin } from '@react-oauth/google';
+import { jwtDecode } from "jwt-decode";
 
 export default function Page() {
   return (
@@ -7,13 +9,28 @@ export default function Page() {
       <header className={styles.roleButton}>Select Your Role</header>
       <p>I am a</p>
       <div className={styles.roleButtons}>
-        <Link href="/student/login">
+        <Link href="">
           <button className={styles.studentButton}>Student</button>
         </Link>
-        <Link href="/instructor/login">
+        <Link href="/instructor/">
           <button className={styles.instructorButton}>Instructor</button>
         </Link>
       </div>
+      <span>
+      <GoogleLogin
+  onSuccess={credentialResponse => {
+    if (credentialResponse?.credential) {
+      const decoded = jwtDecode(credentialResponse.credential);
+      console.log(decoded); // Do something with the decoded credential here this is to be used to confirm Login or registration
+    }
+  }}
+  onError={() => {
+    console.log('Login Failed');
+  }}
+  useOneTap
+/>
+
+      </span>
     </div>
   );
 }
--- app/src/pages/instructor/registration.tsx
+++ app/src/pages/instructor/registration.tsx
@@ -1,62 +1,64 @@
 import { NextPage } from 'next';
+import React from 'react';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import styles from '../../styles/instructor-register.module.css';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+//import { useGoogleOAuth } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
+import { jwtDecode } from "jwt-decode";
 
 const SignUp: NextPage = () => {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const router = useRouter();
-    
-
-    
-  
-    const handleSignUpClick = async () => {
-        // Reference any additional necessary authentification logic here
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const router = useRouter();
 
 
-        if (password !== confirmPassword) {
-            alert('Passwords do not match');
-            return;
-        }
 
-        try {
-            const response = await fetch('/api/addUser', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ firstName, lastName, email, password,role: 'instructor' })
-            });
 
-            if (response.ok) {
-                router.push('/instructor/login');
-            } else {
-                const errorData = await response.json();
-                alert(errorData.error);
-            }
-        } catch (error) {
-            alert('Failed to sign up');
-        }
-    };
+  const handleSignUpClick = async () => {
+    // Reference any additional necessary authentification logic here
 
-    const handleBackClick = () => {
-        // Redirect user to login page
+    if (password !== confirmPassword) {
+      alert('Passwords do not match');
+      return;
+    }
+
+    try {
+      const response = await fetch('/api/addUser', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ firstName, lastName, email, password, role: 'instructor' })
+      });
+
+      if (response.ok) {
         router.push('/instructor/login');
-      };
-    
-    return (
-      <div className={styles.signUp}>
-        <img className={styles.sprintrunner1Icon} alt="Sprintrunner" src="/images/Logo.png" />
-        <div className={styles.signUpChild}></div>
-        <div className={styles.signUpItem}>
+      } else {
+        const errorData = await response.json();
+        alert(errorData.error);
+      }
+    } catch (error) {
+      alert('Failed to sign up');
+    }
+  };
+
+  const handleBackClick = () => {
+    // Redirect user to login page
+    router.push('/instructor/login');
+  };
+
+  return (
+    <div className={styles.signUp}>
+      <img className={styles.sprintrunner1Icon} alt="Sprintrunner" src="/images/Logo.png" />
+      <div className={styles.signUpChild}></div>
+      <div className={styles.signUpItem}>
         <div className={styles.createAnAccount}>Create an Account</div>
-        </div>
-        <div className={`${styles.textInput}`} style={{ top: '200px' }}>
+      </div>
+      <div className={`${styles.textInput}`} style={{ top: '200px' }}>
         <input
           type="text"
           placeholder="First Name"
@@ -64,8 +66,8 @@ const SignUp: NextPage = () => {
           onChange={(e) => setFirstName(e.target.value)}
           className={styles.textInputChild}
         />
-        </div>
-        <div className={`${styles.textInput}`} style={{ top: '270px' }}>
+      </div>
+      <div className={`${styles.textInput}`} style={{ top: '270px' }}>
         <input
           type="text"
           placeholder="Last Name"
@@ -73,8 +75,8 @@ const SignUp: NextPage = () => {
           onChange={(e) => setLastName(e.target.value)}
           className={styles.textInputChild}
         />
-        </div>
-        <div className={`${styles.textInput}`} style={{ top: '340px' }}>
+      </div>
+      <div className={`${styles.textInput}`} style={{ top: '340px' }}>
         <input
           type="email"
           placeholder="Email"
@@ -82,8 +84,8 @@ const SignUp: NextPage = () => {
           onChange={(e) => setEmail(e.target.value)}
           className={styles.textInputChild}
         />
-        </div>
-        <div className={`${styles.textInput}`} style={{ top: '410px' }}>
+      </div>
+      <div className={`${styles.textInput}`} style={{ top: '410px' }}>
         <input
           type="password"
           placeholder="Password"
@@ -91,8 +93,8 @@ const SignUp: NextPage = () => {
           onChange={(e) => setPassword(e.target.value)}
           className={styles.textInputChild}
         />
-        </div>
-        <div className={`${styles.textInput}`} style={{ top: '480px' }}>
+      </div>
+      <div className={`${styles.textInput}`} style={{ top: '480px' }}>
         <input
           type="password"
           placeholder="Confirm Password"
@@ -100,14 +102,25 @@ const SignUp: NextPage = () => {
           onChange={(e) => setConfirmPassword(e.target.value)}
           className={styles.textInputChild}
         />
-        </div>
-        <button className={styles.createEvaluationButton} onClick={handleSignUpClick}>
-        <div className={styles.createEvaluationButtonChild}>Sign Up</div>
-        </button>
-        <p className={styles.pleaseEnterThe}>Please enter the following information:</p>
-        <img className={styles.backIcon} alt="Back" src="/images/Back-Arrow.png" onClick={handleBackClick}/>
       </div>
-    );
-    };
-    
-    export default SignUp;
+      <button className={styles.createEvaluationButton} onClick={handleSignUpClick}>
+        <div className={styles.createEvaluationButtonChild}>Sign Up</div>
+      </button>
+      <span>
+        <GoogleLogin
+          onSuccess={credentialResponse => {
+            console.log(credentialResponse);
+          }}
+          onError={() => {
+            console.log('Login Failed');
+          }}
+          useOneTap
+        />;
+      </span>
+      <p className={styles.pleaseEnterThe}>Please enter the following information:</p>
+      <img className={styles.backIcon} alt="Back" src="/images/Back-Arrow.png" onClick={handleBackClick} />
+    </div>
+  );
+};
+
+export default SignUp;
