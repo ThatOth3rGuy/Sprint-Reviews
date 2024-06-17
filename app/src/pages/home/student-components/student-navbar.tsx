f372ef2bcecee59761309afebf050b5dc07ac9ef
Tithi Soni <tithi2k3@gmail.com>
2024-06-16 22:41:05 -0700

    made instructor templates and updated navbar and header links

--- app/src/pages/home/student-components/student-navbar.tsx
+++ app/src/pages/home/student-components/student-navbar.tsx
@@ -1,39 +1,49 @@
 import type { NextPage } from "next";
 import Link from "next/link";
 import style from "../../../styles/student-components.module.css";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
 //TODO: Add logo and images per button
 
 const StudentNavbar: NextPage = () => {
   const router = useRouter();
 
-  const handleLogoutClick = () => {
-    // Redirect user to login page
-    router.push('/student/login');
-  }
-
   const handleNavigation = (path: string) => {
     router.push(path);
-  }
+  };
 
   return (
     <nav className={style.navbar}>
-      <div className={style.navButton} onClick={() => handleNavigation('/student/dashboard')}>
+      <div
+        className={style.navButton}
+        onClick={() => handleNavigation("/student/dashboard")}
+      >
         Home
       </div>
-      <div className={style.navButton} onClick={() => handleNavigation('/student/all-assignments')}>
+      <div
+        className={style.navButton}
+        onClick={() => handleNavigation("/student/all-assignments")}
+      >
         Assignments
       </div>
-      <div className={style.navButton} onClick={() => handleNavigation('/student/grades')}>
+      <div
+        className={style.navButton}
+        onClick={() => handleNavigation("/student/grades")}
+      >
         Grades
       </div>
-      <div className={style.navButton} onClick={() => handleNavigation('/student/settings')}>
+      <div
+        className={style.navButton}
+        onClick={() => handleNavigation("/student/settings")}
+      >
         Settings
       </div>
       <div className={style.logoutWrapper}>
-        <div className={style.navButton} onClick={handleLogoutClick}>
-          <Link href="#">Logout</Link>
+        <div
+          className={style.navButton}
+          onClick={() => handleNavigation("/student/login")}
+        >
+          Logout
         </div>
       </div>
     </nav>
---
+++ app/src/pages/instructor/profile.tsx
@@ -0,0 +1,16 @@
+import InstructorHeader from "../home/instructor-components/instructor-header";
+import InstructorNavbar from "../home/instructor-components/instructor-navbar";
+
+
+
+export default function Page() {
+  return (
+    <>
+      <br />
+      <br />
+      <br />
+      <InstructorHeader title="Profile"/>
+      <InstructorNavbar/>
+    </>
+  );
+}
