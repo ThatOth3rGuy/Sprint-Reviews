5f7a29042831c9e2bc52b4fc689bc10873a7d7d2
Eric Harrison <hericeh72@gmail.com>
2024-06-19 15:51:35 -0700

    Created template page for course creation

--- app/src/pages/instructor/create-course.tsx
+++ app/src/pages/instructor/create-course.tsx
@@ -1,15 +1,48 @@
+// create-assignment.tsx
+import type { NextPage } from 'next';
+import styles from '../../styles/instructor-courses-creation.module.css';
+import { useRouter } from 'next/router';
+import React, { ChangeEvent, useCallback, useState } from "react";
+
 import InstructorHeader from "../home/instructor-components/instructor-header";
 import InstructorNavbar from "../home/instructor-components/instructor-navbar";
 
+const Courses: NextPage = () => {
+  const [file, setFile] = useState<File | null>(null);
+  const [fileContent, setFileContent] = useState<string | null>(null);
+  const [courseName, setTitle] = useState('');
+  const [institutionName, setDescription] = useState('');
+  const router = useRouter();
 
+  async function handleFileUpload(event: ChangeEvent<HTMLInputElement>) {
+      //Handle student list upload here
+  }
 
-export default function Page() {
+  const onCreateCourseButtonClick = useCallback(async () => {
+    // Handle course creation here
+  }, [courseName, institutionName, fileContent, router]);
+
   return (
     <>
-      <br />
-      <br />
-      <br />
-      <InstructorHeader title="Create Course"/>
+      <InstructorHeader
+        title="Create Course"/>
+      <InstructorNavbar />
+      <div className={styles.container}>
+        <div className={styles.rectangle}>
+          <i style={{width: "368px", position: "relative", fontSize: "35px", display: "flex", fontWeight: "700", fontFamily: "'Inria Serif'", color: "#04124b", textAlign: "left", alignItems: "center", height: "22px",}}>Create a Course</i>
+          <input type="text" placeholder="Course Name" className={styles.textbox} value={courseName} onChange={e => setTitle(e.target.value)} />          
+          <input type="text" placeholder="Institution Name" className={styles.textbox} value={courseName} onChange={e => setDescription(e.target.value)} />
+          <p>Upload Student List: 
+          <input type="file" onChange={handleFileUpload} /></p>          
+          <div className={styles.button} onClick={onCreateCourseButtonClick}>
+            <div />
+            <b>Create Course</b>
+          </div>
+        </div>
+      </div>
+
     </>
   );
 }
+
+export default Courses;
