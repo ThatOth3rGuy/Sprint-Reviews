13b5aedc71214c0b5ddff41e587f038de02d36fa
Eric Harrison <hericeh72@gmail.com>
2024-06-27 15:52:50 -0700

    Made the close button when confirming deleting the course close that popup

--- app/src/pages/components/admin-components/confirm-delete-course.tsx
+++ app/src/pages/components/admin-components/confirm-delete-course.tsx
@@ -3,28 +3,27 @@ import { useCallback } from 'react';
 import styles from '../../../styles/confirm-delete-course.module.css';
 
 export type ConfirmDeleteCourseType = {
-    className?: string;
-    onClose: () => void;
-  }
+  className?: string;
+  onClose: () => void;
+}
 
+const ConfirmDeleteCourse: NextPage<ConfirmDeleteCourseType> = ({ className = "", onClose }) => {
 
+  const onInstructorButtonContainerClick = useCallback(() => {
+    // Add your code here
+  }, []);
 
-const ConfirmDeleteCourse:NextPage<ConfirmDeleteCourseType> = ({ className="" }) => {
-  	
-  	const onInstructorButtonContainerClick = useCallback(() => {
-    		// Add your code here
-  	}, []);
-  	
-  	return (
-    		<div className={[styles.confirmDeleteCourse, className].join(' ')}>
-      			<img className={styles.multiplyIcon} alt="" src="/Images/Close.png" />
-      			<i className={styles.confirmDelete}>Confirm Delete</i>
-      			<b className={styles.onceYouConfirm}>Once you confirm delete, the change will be made permanent. Confirm below to continue.</b>
-      			<div className={styles.instructorButton} onClick={onInstructorButtonContainerClick}>
-        				<div className={styles.instructorButtonChild} />
-        				<b className={styles.createAssignment}>Delete</b>
-      			</div>
-    		</div>);
+  return (
+    <div className={[styles.confirmDeleteCourse, className].join(' ')}>
+      <img className={styles.multiplyIcon} alt="" src="/Images/Close.png" onClick={onClose} />
+      <i className={styles.confirmDelete}>Confirm Delete</i>
+      <b className={styles.onceYouConfirm}>Once you confirm delete, the change will be made permanent. Confirm below to continue.</b>
+      <div className={styles.instructorButton} onClick={onInstructorButtonContainerClick}>
+        <div className={styles.instructorButtonChild} />
+        <b className={styles.createAssignment}>Delete</b>
+      </div>
+    </div>
+  );
 };
 
 export default ConfirmDeleteCourse;
