8f5e5781fe75bc242f55314c2b8a1274fa271831
Eric Harrison <hericeh72@gmail.com>
2024-07-07 13:50:11 -0700

    Adjusting server-test configuration to handle both jest and playwright tests

--- app/server-test/jest.config.ts
+++ app/server-test/jest.config.ts
@@ -1,14 +1,16 @@
+//jest.config.ts
 const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: "./",
+  dir: "../",
 });
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testEnvironment: "jsdom",
+  testMatch: ["<rootDir>/tests-jest/**/*.ts", "<rootDir>/tests-jest/**/*.tsx"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
